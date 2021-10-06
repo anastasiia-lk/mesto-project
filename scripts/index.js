4ae27c9563b43e4f49bcfd95c9ca3c017fcb25de
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2021-10-06 17:09:21 +0300

    Try with popup animayion

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,12 @@ let popup = document.querySelector('.popup');
 let editButton = profile.querySelector('.profile__button-edit');
 let closeButton = popup.querySelector('.popup__button-close');
 function openPopup (){
-  popup.classList.add('popup_opened');
+  popup.classList.remove('popup_is_closed');
+  popup.classList.add('popup_is_opened');
 }
 function closePopup (){
-  popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_is_opened');
+  popup.classList.add('popup_is_closed');
 }
 
 editButton.addEventListener('click', openPopup);
