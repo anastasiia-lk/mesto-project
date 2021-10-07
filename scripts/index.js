2cca9cec9442023bc1867045a6c3bd444e683722
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2021-10-07 21:44:38 +0300

    Added like button

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,16 @@ let popup = document.querySelector('.popup');
 let popupContainer = document.querySelector('.popup__container');
 let editButton = profile.querySelector('.profile__button-edit');
 let closeButton = popup.querySelector('.popup__button-close');
+let likeButtons = document.querySelectorAll('.card__button-like');
+for (let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener("click", function() {
+    likeButtons[i].classList.toggle('active')
+    // if (this.classList.contains('active')) {
+    //   this.classList.remove('active');
+    // }
+    // else this.classList.add('active');
+  });
+}
 function openPopup (){
   popup.classList.add('popup_opened');
   popupContainer.classList.add('popup_opened');
@@ -11,6 +21,5 @@ function closePopup (){
   popup.classList.remove('popup_opened');
   popupContainer.classList.remove('popup_opened');
 }
-
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
