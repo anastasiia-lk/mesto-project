4aab254feb31f428f86b9d50f026c043866e8f1c
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-01-09 17:55:16 +0300

    add constants.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,34 +1,8 @@
 import '../pages/index.css';
+import { initialCards } from "./constants.js";
 import { addPopupCloseListener, setOverlayHandlers, openPopup, closePopup } from './modal.js';
 import { enableFormValidation } from './validate.js';
 import { newPlace, addPlace } from './card.js';
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-  ];
 
 // нажимаем кнопку редактировать профиль
 
